eb0a96f36ac72c4d5d38cef9ae99b0bcec81e5e2
ethanman102 <ethankey.s@yahoo.com>
2024-12-14 16:17:29 -0700

    backend file structure

---
+++ backend/seamsstrangebackend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=30,validators=[MinLengthValidator(3)])
+    email = models.EmailField(max_length=255,unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
