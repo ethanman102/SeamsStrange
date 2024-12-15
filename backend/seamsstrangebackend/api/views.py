b4f4042c9e5ce35b893f4157d7c64464a531d2a2
ethanman102 <ethankey.s@yahoo.com>
2024-12-14 23:36:34 -0700

    simple jwt

--- backend/seamsstrangebackend/api/views.py
+++ backend/seamsstrangebackend/api/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import 
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from .serializers import UserSerializer
+from .models import User
+
 
 # Create your views here.
 class LoginView(APIView):
     def post(self,request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if email is None or password is None:
+            raise ValidationError('Email and Username are required')
+        
+        serializer = UserSerializer(data=request.data, partial=True)
+        if not serializer.is_valid():
+            raise AuthenticationFailed('Email / Password format Invalid')
+        
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('No User exists')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is Incorrect')
+        
+        return Response({'Login Successful'},status=200)
+        
+
+
+
+        
