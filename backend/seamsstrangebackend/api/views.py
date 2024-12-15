a43efffe49b7651800853eec52aeff6ea9a63228
ethanman102 <ethankey.s@yahoo.com>
2024-12-14 17:17:36 -0700

    login api view

--- backend/seamsstrangebackend/api/views.py
+++ backend/seamsstrangebackend/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import 
 
 # Create your views here.
+class LoginView(APIView):
+    def post(self,request):
