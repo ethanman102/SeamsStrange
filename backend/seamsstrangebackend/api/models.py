249c43a94bb6a7e2d46def50f78aa85523632e29
ethanman102 <ethankey.s@yahoo.com>
2024-12-16 01:25:09 -0700

    add colorfield

--- backend/seamsstrangebackend/api/models.py
+++ backend/seamsstrangebackend/api/models.py
@@ -1,8 +1,11 @@
+from colorfield.fields import Colorfield
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+# Custom User Model, user will most likely only be made through createsuperuser manage.py command.
 class User(AbstractUser):
     name = models.CharField(max_length=30,validators=[MinLengthValidator(3)])
     email = models.EmailField(max_length=255,unique=True)
@@ -11,4 +14,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name','username']
+
+
+class Tag()
 
