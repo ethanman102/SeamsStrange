7e54598cfde699bc2aece7b8f878875fb4dad2a7
ethanman102 <ethankey.s@yahoo.com>
2025-01-13 15:10:21 -0700

    initial contact page

--- frontend/seamsstrangefrontend/src/pages/ContactPage.jsx
+++ frontend/seamsstrangefrontend/src/pages/ContactPage.jsx
@@ -1,9 +1,10 @@
 import "../styles/ContactPage.css"
 import noImage from "../assets/seamsstrangelogindesign.jpg"
+import ContactForm from "../components/ContactForm";
 
 const ContactPage = () =>{
     return(
-        <>
+    <div className="contactPage">
     <div className="contactPageHeader">
         <h1>Contact</h1>
         <h2>Seams Strange</h2>
@@ -28,9 +29,15 @@ const ContactPage = () =>{
                 <p className="sellerTextTwo">That's so adorable! We do infact embroider bandannas.<br/>
                 Funny enough I have also done the same for my dog Bree, and she loves it!<br/>Let's talk over the phone so we can get an idea regarding some designs.<br/>
                 <span className="textTimestamp">Wednesday 6:01pm</span></p>
+                <p className="buyerTextThree">Perfect thanks for the help!<br/>
+                I'll be able to hop on the phone in five minutes.<br/>
+                <span className="textTimestamp">Wednesday 6:04pm</span></p>
             </div>
         </div>
-        </>
+        <h2 className="contactPageInquiryText">Looking to get more information on our business? A specific design you had in mind? Pricing?<br/>
+        Don't hesitate to send us your inquiry, visit us in our office, or give us a phone call</h2>
+        <ContactForm/>
+        </div>
     );
 }
 export default ContactPage;
