36a2cebb17dea31cb07619aaa372ea59066d40cc
ethanman102 <ethankey.s@yahoo.com>
2025-01-15 21:42:31 -0700

    useState Refactor

--- frontend/seamsstrangefrontend/src/pages/AdminItemPanel.jsx
+++ frontend/seamsstrangefrontend/src/pages/AdminItemPanel.jsx
@@ -17,11 +17,11 @@ import axios from "axios";
 
 const AdminItemPanel = () => {
 
-    const titleRef = useRef('myTitle');
-    const descriptionRef = useRef('myDescription');
-    const priceRef = useRef(0.00);
-    const linkRef = useRef("http://localhost:8000/");
-    const quantityRef = useRef(0);
+    const [title,setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price,setPrice] = useState(0.00);
+    const [link,setLink] = useState("");
+    const [quantity,setQuantity] = useState(0);
     const [allTags, setAllTags] = useState([]);
     const [currentTags, setCurrentTags] = useState([]);
     const [currentImages,setCurrentImages] = useState([]);
@@ -37,20 +37,20 @@ const AdminItemPanel = () => {
     // case when navigating to edit the item.
     useEffect(() =>{
         if (state && state.id){
-            titleRef.current = state.title;
-            descriptionRef.current = state.description;
-            priceRef.current = state.price
-            linkRef.current = state.etsyURL;
-            quantityRef.current = state.quantity;
+            setTitle(state.title);
+            setDescription(state.description);
+            setPrice(state.price);
+            setLink(state.etsyURL);
+            setQuantity(state.quantity);
             setCurrentImages(state.images);
             setCurrentTags(state.tags);
             setViewMode(View.EDIT);
         }else{
-            titleRef.current = '';
-            descriptionRef.current = '';
-            priceRef.current = 0.00;
-            linkRef.current = '';
-            quantityRef.current = 0;
+            setTitle('');
+            setDescription('');
+            setPrice(0.00);
+            setLink('');
+            setQuantity(0);
             setCurrentImages([]);
             setCurrentTags([]);
             setViewMode([]);
@@ -62,23 +62,23 @@ const AdminItemPanel = () => {
     },[]);
 
     const handleTitleChange = (titleText) =>{
-        titleRef.current = titleText;
+        setTitle(titleText);
     }
 
     const handleDescriptionChange = (descriptionText) =>{
-        descriptionRef.current = descriptionText;
+        setDescription(descriptionText);
     }
 
     const handlePriceChange = (itemPrice) =>{
-        priceRef.current = itemPrice;
+        setPrice(itemPrice);
     }
 
     const handleLinkChange = (itemLink) =>{
-        linkRef.current = itemLink;
+        setLink(itemLink);
     }
 
     const handleQuantityChange = (itemQuantity) =>{
-        quantityRef.current = itemQuantity;
+        setQuantity(itemQuantity);
     }
 
     const handleTagChange = (tagList) =>{
@@ -99,19 +99,36 @@ const AdminItemPanel = () => {
         navigate('/admin/items/');
     }
     
+    const onReset = () => {
+        setTitle(state.title);
+        setDescription(state.description);
+        setPrice(state.price);
+        setLink(state.etsyURL);
+        setQuantity(state.quantity);
+        setCurrentImages(state.images);
+        setCurrentTags(state.tags);
+    }
+
+
+
     const createItem = () => {
         // Validate here to reduce round trip time.
+        console.log('hi');
         var data = {}
-        if (quantityRef.current < 0) return;
-        data.quantity = Number(quantityRef.current);
-        if (priceRef.current < 0) return;
-        data.price = parseFloat(priceRef.current).toFixed(2); // this line gives error december 31 8pm
-        if (!titleRef.current) return;
-        data.title = titleRef.current;
-        if (!descriptionRef.current) return;
-        data.description = descriptionRef.current;
-        if (!linkRef) linkRef.current = "";
-        data.etsy_url = linkRef.current;
+        if (quantity < 0) return;
+        console.log('boo')
+        data.quantity = Number(quantity);
+        console.log('oop')
+        if (price < 0) return;
+        console.log(price);
+        data.price = parseFloat(price).toFixed(2); // this line gives error december 31 8pm
+        if (!title) return;
+        data.title = title;
+        console.log('here');
+        if (!description) return;
+        data.description = description;
+        if (!link) setLink("");
+        data.etsy_url = link;
         data.tags = currentTags;
         data.images = []
 
@@ -157,13 +174,22 @@ const AdminItemPanel = () => {
         <div className="tagAndInputContainer">
             <div className="adminItemsTextualInputs">
                 <h2 className="itemViewMode">Mode: {viewMode === View.VIEW ? "Create" : "Edit"}</h2>
-                {viewMode === View.EDIT ? <button className="disableModeButton" onClick={() => onDisableEdit()}>Disable Edit Mode</button> : ''}
-                <ItemTitleInput titleText={titleRef.current} handleTitle={handleTitleChange}/>
+                {
+                    viewMode === View.EDIT ? <>
+                    <button className="disableModeButton" onClick={() => onDisableEdit()}>Disable Edit Mode</button>
+                    <p className="editButtonsPrompt">To reset the all changes you made you can utilize the reset button.<br/>To delete an item from the shop press the delete button.</p>  
+                    <div className="editOptionButtons">
+                        <button className="itemResetButton" onClick={onReset}>Reset</button>
+                        <button className="itemDeleteButton">DELETE</button>
+                    </div>
+                        </>: ''
+                }
+                <ItemTitleInput titleText={title} handleTitle={handleTitleChange}/>
 
-                <ItemDescriptionInput description={descriptionRef.current} handleDescription={handleDescriptionChange}/>
+                <ItemDescriptionInput description={description} handleDescription={handleDescriptionChange}/>
                 <div className="priceLinkAvailabilityContainer">
-                    <ItemPriceInput itemPrice={priceRef.current} handlePrice={handlePriceChange}/>
-                    <ItemQuantityInput itemQuantity={quantityRef.current} handleQuantity={handleQuantityChange}/>
+                    <ItemPriceInput itemPrice={price} handlePrice={handlePriceChange}/>
+                    <ItemQuantityInput itemQuantity={quantity} handleQuantity={handleQuantityChange}/>
                 </div>
                 <h2 className="attachedTagsHeader">Attached Tags 🏷️</h2>
                 <p className="itemCreationInputPrompt">
@@ -176,7 +202,7 @@ const AdminItemPanel = () => {
                 To detach an image associated to the item click on the delete button at the top right corner.</p>
                 {currentImages.length > 0 && <ImageSlider images={currentImages} mode={View.EDIT} handleRemove={onDeleteImage}/>}
                 <ImageUploader handleUpload={onUpload}/>
-                <ItemLinkInput itemLink={linkRef.current} handleLink={handleLinkChange}/>
+                <ItemLinkInput itemLink={link} handleLink={handleLinkChange}/>
                 <button className="createItemButton" onClick={createItem}>Save</button>
             </div>
             <TagFilter purpose="Attach a " filterFunction={handleTagChange} tags={allTags} currentSelection={currentTags}/>
