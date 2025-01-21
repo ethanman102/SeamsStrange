f8c3e6991ab96165124198ffc5482acfada183d1
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2025-01-21 12:24:06 -0700

    frontend production

--- backend/seamsstrangebackend/seamsadmin/settings.py
+++ backend/seamsstrangebackend/seamsadmin/settings.py
@@ -107,7 +107,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION' : True,
     'UPDATE_LAST_LOGIN' : False,
     'ALGORITHM' : 'HS256',
-    'SIGNING_KEY' : SECRET_KEY,
+    'SIGNING_KEY' : env('SIGNING_KEY'),
     'VERIFYING_KEY' : None,
     'AUDIENCE' : None,
     'ISSUER' : None,
