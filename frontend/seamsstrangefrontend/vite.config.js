f3109aeba1fde7b4c15fee342e8220df369f4b21
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2025-01-21 12:56:01 -0700

    vite port

--- frontend/seamsstrangefrontend/vite.config.js
+++ frontend/seamsstrangefrontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173, // Use Heroku's $PORT or default to 4173
+    host: true, // Expose Vite to the public network
+  },
 })
