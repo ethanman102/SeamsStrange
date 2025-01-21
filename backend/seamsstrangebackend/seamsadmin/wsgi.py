65c2f66385dd7c56cb04924ff5ebc5e0253720b6
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2025-01-20 19:17:27 -0700

    new settings

--- backend/seamsstrangebackend/seamsadmin/wsgi.py
+++ backend/seamsstrangebackend/seamsadmin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seamsstrangebackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seamsstrangebackend.seamsadmin.settings')
 
 application = get_wsgi_application()
