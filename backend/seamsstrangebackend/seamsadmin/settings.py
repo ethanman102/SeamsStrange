086115334165e6272e1d00e0ba17012d4aa6976e
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2025-01-30 13:10:39 -0700

    Update settings.py

--- backend/seamsstrangebackend/seamsadmin/settings.py
+++ backend/seamsstrangebackend/seamsadmin/settings.py
@@ -79,6 +79,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://seamsstrange.com",
     "https://www.seamsstrange.com",
     "http://localhost:4173",
+
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -86,6 +87,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://seamsstrange.com",
     "https://www.seamsstrange.com",
     "http://localhost:4173",
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
