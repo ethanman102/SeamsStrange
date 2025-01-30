d0f0887603d1c23e7162d2bc4f3b098b3dafe8bd
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2025-01-30 14:33:51 -0700

    Update settings.py

--- backend/seamsstrangebackend/seamsadmin/settings.py
+++ backend/seamsstrangebackend/seamsadmin/settings.py
@@ -235,3 +235,9 @@ DEFAULT_FILE_STORAGE = 'storages.backend.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Redirect HTTP to HTTPS in production
+SECURE_SSL_REDIRECT = True  # Enforces HTTPS
